3058f4e2792b6802ad035c038d5e64e424834154
ohaiitspetra <43091834+ohaiitspetra@users.noreply.github.com>
2018-09-13 17:03:22 -0400

    My version

--- assignments/data-types.py
+++ assignments/data-types.py
@@ -2,31 +2,27 @@
 
 # On the line below, create a variable called four and assign
 # the value 2 plus 2 to it.
-
-
+four=2+2
+type(four)
 # Create a variable called george that stores the type of True
-
-
+george=(6)
+bool(george)
+True
 # Create a variable called yarn that stores the value of the variable four
 # from the first exercise as a string
-
-
+yarn=four
 # What does bool("0") evaluate to?
-
-
+True
 # Create a list containing four, george, and yarn
-
+list(four,george,yarn)
 
 # Calculate 3 to the 13th power and store the result as the variable big
-
-
+3**13
+1594323
+big=1594323
 # What is the value of True + True? Write your answer in a comment
-
-
+2
 # What is the value of "one" + "one"?
-
-
+'oneone'
 # What is the value of [1, 2, 3] + [4, 5, 6]?
-
-
-
+[1,2,3,4,5,6]
