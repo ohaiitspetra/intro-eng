4aa0f345c1af0c4aca88290bea07100ed9fff648
Dave Enrico <denrico@northbergen.k12.nj.us>
2018-09-13 10:53:15 -0400

    initial commit

---
+++ assignments/data-types.py
@@ -0,0 +1,32 @@
+# PRACTICE
+
+# On the line below, create a variable called four and assign
+# the value 2 plus 2 to it.
+
+
+# Create a variable called george that stores the type of True
+
+
+# Create a variable called yarn that stores the value of the variable four
+# from the first exercise as a string
+
+
+# What does bool("0") evaluate to?
+
+
+# Create a list containing four, george, and yarn
+
+
+# Calculate 3 to the 13th power and store the result as the variable big
+
+
+# What is the value of True + True? Write your answer in a comment
+
+
+# What is the value of "one" + "one"?
+
+
+# What is the value of [1, 2, 3] + [4, 5, 6]?
+
+
+
---
+++ notes/intro.md
@@ -0,0 +1,3 @@
+# Working with the Command Line in Python
+
+
